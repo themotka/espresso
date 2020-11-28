6c8720fe5d9ae8ecc9389c1201c739973a846f89
themotka <77videman@mail.ru>
2020-11-28 22:41:30 +0700

    finish

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import sqlite3
 import sys
 
-from PyQt5 import uic  # Импортируем uic
+from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QHeaderView, QTableWidgetItem
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui', self)  # Загружаем дизайн
+        uic.loadUi('main.ui', self)
         self.con = sqlite3.connect("coffee.sqlite")
         self.load_table()
 
     def load_table(self):
-        # Получим результат запроса,
-        # который ввели в текстовое поле
+
         query = "SELECT * FROM coffee"
         res = self.con.cursor().execute(query).fetchall()
         self.tableWidget.setColumnCount(7)
         self.tableWidget.setRowCount(0)
-        # Заполняем таблицу элементами
         for i, row in enumerate(res):
             self.tableWidget.setRowCount(
                 self.tableWidget.rowCount() + 1)
